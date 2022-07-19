8a04a10d81c62f0ffe071f4a6e2f35e5e0f598a4
matthcol <matthias.colin@gmail.com>
2022-07-19 16:03:26 +0200

    config db test

---
+++ TestApiNFL/LearnEntityFramework/NFLDbContextTest.cs
@@ -0,0 +1,22 @@
+﻿using ApiNFL.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApiNFL.LearnEntityFramework
+{
+    public class NFLDbContextTest
+    {
+        protected DbContextOptions<NFLDbContext> ContextOptions { get; set; }
+
+        protected NFLDbContextTest(DbContextOptions<NFLDbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            // init some data
+        }
+
+        
+    }
+}
--- TestApiNFL/LearnEntityFramework/TeamCrudTest.cs
+++ TestApiNFL/LearnEntityFramework/TeamCrudTest.cs
@@ -1,16 +1,29 @@
+using ApiNFL.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
 
 namespace TestApiNFL.LearnEntityFramework
 {
-    public class TeamCrud
+    public class TeamCrudTest: NFLDbContextTest
     {
+        public TeamCrudTest() : base(
+            new DbContextOptionsBuilder<NFLDbContext>()
+            .UseInMemoryDatabase("dbnfltest1")
+            .Options
+        )
+        {
+        }
+
         [Theory]
         [InlineData("Seahawks")]
         [InlineData("Sea")]
         public void TestSave(string name)
         {
+            using (var dbContext = new NFLDbContext(ContextOptions))
+            {
 
+            }
         }
 
         [Fact]
